7166a3f7ab5525e5cecce6dc50ddb63100bb665b
Marius Biller <b.majus@web.de>
2023-09-09 21:55:20 +0200

    export definitions into library

--- main.c
+++ main.c
@@ -1,26 +1,5 @@
-#include <stdio.h>
 #include <string.h>
-
-// prototype declaration of main structure
-typedef struct ANIMEL_TYPE ANIMEL_TYPE;
-
-// function pointer type for animel sound
-typedef void (*SoundFunction)( ANIMEL_TYPE*);
-
-// the animel structure
-typedef struct ANIMEL_TYPE {
-    char name[20];
-    char sound[20];
-    SoundFunction makeSound; // Function pointer for making a sound
-} ANIMEL_TYPE;
-
-// function to make an animel sound
-void make_sound(ANIMEL_TYPE* animel) {
-    printf("the '%s' says '%s'\n", animel->name, animel->sound);
-}
-
-//----------------------------------------------------------------------
-
+#include "lib/c-objects/c-objects.h"
 
 int main() {
 
